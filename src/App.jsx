9062b9c37677b20dac4c65d6c0dd235a51b9f859
flaviogotojp <flaviogotojp@gmail.com>
2026-08-08 04:41:10 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>Driver Finance</h1>
+      <p>Sistema de gestão financeira para motoristas.</p>
+    </div>
+  );
+}
+
+export default App;
